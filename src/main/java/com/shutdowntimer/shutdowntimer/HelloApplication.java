d81c66fb2ad9480fd26558c3efa2cecf5d4f4b95
H-Kann <hamzakannouch@gmail.com>
2024-10-28 02:26:10 +0100

    feat: Updated the UI feat: Implemented AtlantaFX themes

--- src/main/java/com/shutdowntimer/shutdowntimer/HelloApplication.java
+++ src/main/java/com/shutdowntimer/shutdowntimer/HelloApplication.java
@@ -1,5 +1,6 @@
 package com.shutdowntimer.shutdowntimer;
 
+import atlantafx.base.theme.Dracula;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -8,16 +9,18 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class HelloApplication extends Application {
-    @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+
+    public static void main(String[]args){
+        launch(args);
     }
 
-    public static void main(String[] args) {
-        launch();
+    @Override
+    public void start(Stage stage) throws IOException {
+        Application.setUserAgentStylesheet(new Dracula().getUserAgentStylesheet());
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Shutdown Timer!");
+        stage.setScene(scene);
+        stage.show();
     }
 }
